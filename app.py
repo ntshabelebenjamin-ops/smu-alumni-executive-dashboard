e90a31f8f07181d09571dd3ee041f6ee1babe23f
ntshabelebenjamin-ops <ntshabelebenjamin@gmail.com>
2026-06-05 17:31:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import streamlit as st
 
+@st.cache_data
+def load_data():
+    return pd.read_excel(
+        "SMU_Alumni_Short_Survey_ANALYTICS_READY.xlsx"
+    )
+
+df = load_data()
