bc8bd231f6d80507b71cf05981a2f0cca4b35695
ntshabelebenjamin-ops <ntshabelebenjamin@gmail.com>
2026-06-05 20:59:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,6 @@ import plotly.express as px
 # ==================================================
 
 
-st.set_page_config(
-    page_title="SMU Alumni Executive Dashboard",
-    layout="wide"
-)
-
 
 
 st.caption(
