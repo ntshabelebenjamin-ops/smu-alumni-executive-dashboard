8524a6a080ea8247d1717fdedb5b9a62fdc04b5f
ntshabelebenjamin-ops <ntshabelebenjamin@gmail.com>
2026-06-05 18:28:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,10 +30,9 @@ SMU_ORANGE = "#F37021"
 
 @st.cache_data
 def load_data():
-return pd.read_excel(
-"SMU_Alumni_Short_Survey_ANALYTICS_READY.xlsx"
-)
-
+    return pd.read_excel(
+        "SMU_Alumni_Short_Survey_ANALYTICS_READY.xlsx"
+    )
 df = load_data()
 
 # --------------------------------------------------
